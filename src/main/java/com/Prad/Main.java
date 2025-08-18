e9034eb575835d44ccdec5cae8523d216e3de616
Pradthelegend <pradeep.studies06@gmail.com>
2025-08-17 22:58:05 -0400

    Added code related to Lecture 136 just made the code look neat.

--- src/main/java/com/Prad/Main.java
+++ src/main/java/com/Prad/Main.java
@@ -10,21 +10,28 @@ public class Main {
     public static void main(String[] args) {
 
         Students s1 = new Students();
-        s1.setName("King David");
-        s1.setRollNo(3);
-        s1.setAge(30);
+        s1.setName("Paul");
+        s1.setRollNo(4);
+        s1.setAge(35);
 
-        Configuration config = new Configuration();
-        config.addAnnotatedClass(com.Prad.Students.class);// This is kinda like : “Hey Hibernate, this Java class is special — it’s an entity mapped to a database table.
-                                                          //Go look at its annotations (@Entity, @Table, @Column, etc.) and figure out how to store and retrieve it.”
-        config.configure();// This will load the configuration xml.
-        SessionFactory sf = config.buildSessionFactory();
+//        Configuration config = new Configuration();
+//        config.addAnnotatedClass(com.Prad.Students.class);// This is kinda like : “Hey Hibernate, this Java class is special — it’s an entity mapped to a database table.
+//                                                          //Go look at its annotations (@Entity, @Table, @Column, etc.) and figure out how to store and retrieve it.”
+//        config.configure();// This will load the configuration xml.
+
+
+        SessionFactory sf = new Configuration() // We are doint the same thing as above just making it look neat.
+                .addAnnotatedClass(com.Prad.Students.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction(); //You need a transaction for any write operation in Hibernate.
 
-        session.save(s1);
+        session.persist(s1);
         transaction.commit();//Executes the SQL and actually writes the data to the database table.
         System.out.println(s1);
+        session.close();
+        sf.close();
     }
 }
