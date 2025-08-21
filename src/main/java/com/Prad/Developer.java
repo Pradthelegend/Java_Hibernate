dc4a7c8116967cc26d80e4c7f7b573b8c30afc8d
Pradthelegend <pradeep.studies06@gmail.com>
2025-08-20 22:57:37 -0400

    This file is part of lecture 140 its a supporting file.

--- src/main/java/com/Prad/Developer.java
+++ src/main/java/com/Prad/Developer.java
@@ -1,4 +1,57 @@
 package com.Prad;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+//Note:This is part of lecture 140.
+@Entity
 public class Developer {
+
+    @Id
+    private int dID;
+    private String dName;
+    private String dtech;
+    private Laptop laptop;
+
+    public int getdID() {
+        return dID;
+    }
+
+    public void setdID(int dID) {
+        this.dID = dID;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getDtech() {
+        return dtech;
+    }
+
+    public void setDtech(String dtech) {
+        this.dtech = dtech;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "dID=" + dID +
+                ", dName='" + dName + '\'' +
+                ", dtech='" + dtech + '\'' +
+                ", laptop=" + laptop +
+                '}';
+    }
 }
--- src/main/java/com/Prad/Laptop.java
+++ src/main/java/com/Prad/Laptop.java
@@ -1,4 +1,44 @@
 package com.Prad;
 
+import jakarta.persistence.Embeddable;
+//Note:This is linked to "Developer" class and this is part of lecture 140.
+@Embeddable // When you mark a class as "Embeddable" the class attributes like "brand", "ram" etc get added as columns while creating the table. Refer "Developer" class you'll understand.
 public class Laptop {
+
+    private String brand;
+    private String model;
+    private int ram;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", ram='" + ram + '\'' +
+                '}';
+    }
 }
